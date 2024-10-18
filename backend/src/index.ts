8f88b6fc2f78468f473a8f88f536eb5fd036990d
Pragadeesh122 <pragan189@gmail.com>
2024-10-17 20:30:40 -0500

    added firebase to the project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,12 @@
 import express from "express";
 import z from "zod";
+import admin from "firebase-admin";
+import {ServiceAccount} from "firebase-admin";
+import serviceAccount from "../firebaseKey.json";
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as ServiceAccount),
+});
 
 const app = express();
 app.use(express.json());
@@ -8,6 +15,20 @@ app.get("/health", (req, res) => {
   res.json({
     Message: "Server is running ",
   });
+});
+
+app.get("/test-firebase", async (req, res) => {
+  try {
+    const defaultAuth = admin.auth();
+    const userRecord = await defaultAuth.createUser({
+      email: "test@example.com",
+      password: "secretPassword",
+    });
+    res.json({message: "Firebase Admin is working", userId: userRecord.uid});
+  } catch (error) {
+    console.error("Error testing Firebase:", error);
+    res.status(500).json({error: "Firebase Admin test failed"});
+  }
 });
 
 app.listen(3000, () => {
